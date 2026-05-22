bdfb0131c292cca63b0e85584ee6957751898dfc
fadhilahsyaban6 <fadhilahsyaban6@gmail.com>
2026-05-22 10:41:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,26 @@ else:
 
 if st.button("hahaha", type="tertiary"):
     st.write("HAYUKKK")
+
+import streamlit as st
+
+def get_user_name():
+    return 'John'
+
+# ------------------------------------------------
+# Want people to see this part of the code...
+
+def get_punctuation():
+    return '!!!'
+
+greeting = "Hi there, "
+user_name = get_user_name()
+punctuation = get_punctuation()
+
+st.write(greeting, user_name, punctuation)
+
+# ...up to here
+# ------------------------------------------------
+
+foo = 'bar'
+st.write('Done!')
