d11fe80c2335befd61633790c5667ade7e3cd312
fadhilahsyaban6 <fadhilahsyaban6@gmail.com>
2026-05-22 10:36:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ st.title("🎈 fADHIL WORLD")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.button("Reset", type="primary")
+if st.button("ROR"):
+    st.write("NAON")
+else:
+    st.write("POKE")
+
+if st.button("CAPA", type="tertiary"):
+    st.write("HAYUKKK")
